b6de91a75acd8d175bda22496336d604450a19af
veeru2005 <163710790+veeru2005@users.noreply.github.com>
2026-02-06 14:55:06 +0530

    backend

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5005;
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], // Allow frontend
+    origin: ['http://localhost:5173', 'https://kluesports.in'], // Allow frontend
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
